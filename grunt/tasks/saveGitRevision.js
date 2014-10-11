de4dd228356acfb2190f8859285891295280cd5e
dzuluagaapigee <dzuluaga@apigee.com>
2014-10-11 15:38:41 -0400

    replacement of git-rev for gift due to windows incompatibility

--- grunt/tasks/saveGitRevision.js
+++ grunt/tasks/saveGitRevision.js
@@ -1,17 +1,17 @@
 module.exports = function(grunt) {
 	'use strict';
 	grunt.registerTask('saveGitRevision', function() {
-		var git = require('git-rev')
+		var git = require('gift');
+		var repo = git(".")
 		var done = this.async();
-		git.log(function (array) {
-		  var commit = array[0]
-		  var gitRevision = "";
-		  var os = require("os");
-		  if(commit) {
-		  	gitRevision = "git commit: " + commit[0].substring(0,7) + " by " + commit[3] + " on " + os.hostname();
-		  	grunt.option('gitRevision', gitRevision);
-		  }
-		  done();
-		})
-	});
+		var os = require("os");
+		repo.current_commit( function(err, commit){
+			var gitRevision = "";
+		  	if(commit) {
+		  		gitRevision = "git commit: " + commit.id.substring(0,7) + " by " + commit.author.name + " on " + os.hostname();
+		  		grunt.option('gitRevision', gitRevision);
+			}
+			done();
+		}
+		)});
 };
